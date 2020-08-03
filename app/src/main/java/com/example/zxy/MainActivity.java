ad6cfd5604d31db72bc11f5b714d59fa0f5c264f
morteza omidipoor <omidiporm@gmail.com>
2020-08-04 01:59:55 +0430

    add zxy tile with runtime permission management.

--- app/src/main/java/com/example/zxy/MainActivity.java
+++ app/src/main/java/com/example/zxy/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.zxy;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 import com.mapbox.mapboxsdk.Mapbox;
@@ -14,49 +18,77 @@ import com.mapbox.mapboxsdk.style.sources.RasterSource;
 import com.mapbox.mapboxsdk.style.sources.TileSet;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.File;
+
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
 
 
 /**
  * The most basic example of adding a map to an activity.
  */
-public class MainActivity extends AppCompatActivity {
+@RuntimePermissions
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private MapView mapView;
+    MapView mapView;
+    MapboxMap mapboxMap;
+    Button btnAddTiles;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Mapbox access token is configured here. This needs to be called either in your application
-        // object or in the same activity which contains the mapview.
         Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
 
-        // This contains the MapView in XML and needs to be called after the access token is configured.
         setContentView(R.layout.activity_main);
-
         mapView = findViewById(R.id.mapView);
+        btnAddTiles = findViewById(R.id.addTiles);
         mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(new OnMapReadyCallback() {
+        mapView.getMapAsync(this);
+
+        btnAddTiles.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onMapReady(@NonNull final MapboxMap mapboxMap) {
+            public void onClick(View view) {
+                MainActivityPermissionsDispatcher.addExternalZXYWithPermissionCheck(MainActivity.this);
 
-                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
-                    @Override
-                    public void onStyleLoaded(@NonNull Style style) {
-                        addExternalZXY(mapboxMap);
-
-                    }
-                });
             }
         });
+
     }
 
-    private void addExternalZXY(MapboxMap mapboxMap) {
-        File file = new File("/sdcard/tiles/0/0/0.png");
+    @Override
+    public void onMapReady(@NonNull MapboxMap mapboxMap) {
+            this.mapboxMap = mapboxMap;
+
+        mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+            @Override
+            public void onStyleLoaded(@NonNull Style style) {
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // NOTE: delegate the permission handling to generated method
+        MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+
+
+    @NeedsPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+    void addExternalZXY() {
+        // File file = new File("/sdcard/tiles/0/0/0.png");
+        File file = new File("/sdcard/01-NGO_GIS/Tiles");
         if (file.exists()){
             Log.d("MyErr", file.getAbsolutePath());
         }
@@ -70,8 +102,29 @@ public class MainActivity extends AppCompatActivity {
             RasterLayer rasterLayer = new RasterLayer("testTileLayer",
                     "testTileSource");
             mapboxMap.getStyle().addLayer(rasterLayer);
+            btnAddTiles.setVisibility(View.INVISIBLE);
         }
     }
+
+    @OnShowRationale(Manifest.permission.READ_EXTERNAL_STORAGE)
+    void showRationaleForCamera(final PermissionRequest request) {
+        new AlertDialog.Builder(this)
+                .setMessage(R.string.permission_external_storage_denied)
+                .setPositiveButton("button_allow", (dialog, button) -> request.proceed())
+                .setNegativeButton("button_deny", (dialog, button) -> request.cancel())
+                .show();
+    }
+
+    @OnPermissionDenied(Manifest.permission.READ_EXTERNAL_STORAGE)
+    void showDeniedForCamera() {
+        Toast.makeText(this, "OnPermissionDenied", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnNeverAskAgain(Manifest.permission.READ_EXTERNAL_STORAGE)
+    void showNeverAskForCamera() {
+        Toast.makeText(this, "OnNeverAskAgain", Toast.LENGTH_SHORT).show();
+    }
+
 
     // Add the mapView lifecycle to the activity's lifecycle methods
     @Override
@@ -115,4 +168,6 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
         mapView.onSaveInstanceState(outState);
     }
+
+
 }
